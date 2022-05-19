d05308e7336760abf274dc1228749cbcdfbbceba
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-19 12:47:10 -0400

    Test: has hash of 16 cells

---
+++ spec/board_spec.rb
@@ -0,0 +1,45 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require 'rspec'
+
+
+
+RSpec.describe Board do
+
+  before(:each) do
+    # cell_letter_array = ["A","B","C","D"]
+    # cell_integer_array = ["1","2","3","4"]
+    cells = {
+      "A1" => Cell.new("A1"),
+      "A2" => Cell.new("A2"),
+      "A3" => Cell.new("A3"),
+      "A4" => Cell.new("A4"),
+      "B1" => Cell.new("B1"),
+      "B2" => Cell.new("B2"),
+      "B3" => Cell.new("B3"),
+      "B4" => Cell.new("B4"),
+      "C1" => Cell.new("C1"),
+      "C2" => Cell.new("C2"),
+      "C3" => Cell.new("C3"),
+      "C4" => Cell.new("C4"),
+      "D1" => Cell.new("D1"),
+      "D2" => Cell.new("D2"),
+      "D3" => Cell.new("D3"),
+      "D4" => Cell.new("D4")
+    }
+  end
+
+  it 'exists' do
+    board = Board.new
+
+    expect(board).to be_instance_of(Board)
+  end
+
+  it 'has a hash of 16 cells' do
+    board = Board.new
+
+    expect(board.cells.length).to eq(16)
+  end
+
+end
