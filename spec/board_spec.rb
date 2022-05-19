9b6e021ad6bf1169792c84d839586dabbbeddda6
Tyler Caudill <teecaud@hotmail.com>
2022-05-19 12:35:55 -0500

    test: placement length validity

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -40,4 +40,15 @@ RSpec.describe Board do
     expect(board.valid_coordinate?("A22")).to be false
   end
 
+  it "checks placement length validity" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
+     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
+      expect(board.valid_placement?(submarine, ["A2", "A3"])).to be true
+
+  end
+
 end
