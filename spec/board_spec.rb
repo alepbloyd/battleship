dc0bdf8168db7aee4aa803b87873aa9b5de1bcba
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-19 12:54:06 -0400

    Test: return true for valid coordinate

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -19,4 +19,11 @@ RSpec.describe Board do
     expect(board.cells.length).to eq(16)
   end
 
+  it 'returns true for valid coordinate' do
+    board = Board.new
+
+    expect(board.valid_coordinate?("A1")).to be true
+
+  end
+
 end
