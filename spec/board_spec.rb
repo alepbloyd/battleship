d5539c1790afbbf5544c06cba3b8a01f2628fb89
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-20 15:44:57 -0400

    Test: add additional expectations in ship overlap check

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -103,8 +103,12 @@ RSpec.describe Board do
     board.place(cruiser, ["A1", "A2", "A3"])
 
     submarine = Ship.new("Submarine", 2)
+    submarine_2 = Ship.new("Submarine", 2)
+    submarine_3 = Ship.new("Submarine", 2)
 
     expect(board.valid_placement?(submarine, ["A1","B1"])).to be false
+    expect(board.valid_placement?(submarine_2, ["A1","A2"])).to be false
+    expect(board.valid_placement?(submarine_3, ["D1","D2"])).to be true
 
   end
 
