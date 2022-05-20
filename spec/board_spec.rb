201d17f32602ffe8dab2bffded0a1764b4f23abd
Tyler Caudill <teecaud@hotmail.com>
2022-05-20 16:26:30 -0500

    test: renders with '.' when initiated.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -109,7 +109,15 @@ RSpec.describe Board do
     expect(board.valid_placement?(submarine, ["A1","B1"])).to be false
     expect(board.valid_placement?(submarine_2, ["A1","A2"])).to be false
     expect(board.valid_placement?(submarine_3, ["D1","D2"])).to be true
+  end
 
+  it "renders with '.' when initiated" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    board.render
+
+    expect(board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
   end
 
 
