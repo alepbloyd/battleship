ae37520c9c6406cad4c33077db84b8c8b4798434
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-20 22:42:44 -0400

    Feat: number_of_rows

--- lib/computer_placement.rb
+++ lib/computer_placement.rb
@@ -1,6 +1,20 @@
 class ComputerPlacement
 
-  def initialize
+  attr_reader :board
+
+  def initialize(board)
+    @board = board
+  end
+
+  def number_of_rows
+    cells_key_array = []
+
+    (@board.cells.keys).each do |key|
+      cells_key_array << key[0]
+    end
+
+    unique_number_count = cells_key_array.uniq.count
+    unique_number_count
   end
 
 end
