d5e88fc87048875c7b75165a0cb1478ef1bb930f
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-20 23:52:42 -0400

    Feat: horizontal placements

--- lib/computer_placement.rb
+++ lib/computer_placement.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ComputerPlacement
 
   attr_reader :board
@@ -6,9 +8,30 @@ class ComputerPlacement
     @board = board
   end
 
+  def horizontal_placements(gameboard,ship)
+    valid_horizontal_placements = []
 
-  # def horizontal_placements(gameboard,ship)
-  #
-  # end
+    rows = gameboard.number_of_rows
+    columns = gameboard.number_of_columns
+    length = ship.length
+
+    gameboard.cells.each do |cell|
+      coordinate_array = []
+      start_cell = cell[0]
+      start_cell_letter = cell[0][0]
+      start_cell_number = cell[0][-1..1].to_i
+
+      loop_start_number = start_cell_number - 1
+      length.times do
+        coordinate_array << "#{start_cell_letter}#{loop_start_number += 1}"
+      end
+
+      if coordinate_array.all? {|cell| gameboard.valid_coordinate?(cell)}
+        valid_horizontal_placements << coordinate_array
+      end
+    end
+    valid_horizontal_placements
+
+  end
 
 end
