5d6460cba619f3f9f1b0304be4b2f041f48c66ce
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-23 18:05:16 -0400

    Refactor: player prompts

--- lib/player_placement.rb
+++ lib/player_placement.rb
@@ -10,7 +10,7 @@ attr_reader :board, :ships_to_be_placed
   end
 
   def opening_prompt
-    return "You now need to lay out your #{@ships_to_be_placed.length} ships.\n The #{@ships_to_be_placed[0].name} is #{@ships_to_be_placed[0].length} units long and the #{@ships_to_be_placed[1].name} is #{@ships_to_be_placed[1].length} units long."
+    return "You now need to lay out your #{@ships_to_be_placed.length} ships. \nThe #{@ships_to_be_placed[0].name} is #{@ships_to_be_placed[0].length} units long and the #{@ships_to_be_placed[1].name} is #{@ships_to_be_placed[1].length} units long."
   end
 
   def display_board(render_status = false)
@@ -21,16 +21,16 @@ attr_reader :board, :ships_to_be_placed
     "Enter the squares for the #{@ships_to_be_placed[0].name} (#{@ships_to_be_placed[0].length} spaces):"
   end
 
-  def user_input_array(input)
+  def user_input_array
     input = gets.chomp
     input.split(" ")
   end
 
-  def check_user_input(ship, input_array)
-    check_input = user_input_array(input_array)
+  def check_user_input(ship)
+    check_input = user_input_array
     while @board.valid_placement?(ship, check_input) == false do
       puts "Those are invalid coordinates. Please try again:"
-      check_input = user_input_array(input_array)
+      check_input = user_input_array
     end
     @board.place(ship, check_input)
     @ships_to_be_placed.shift
