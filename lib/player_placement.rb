aa7270efcab9c575bfac4f97c5ae3c736ccaee1a
Tyler Caudill <teecaud@hotmail.com>
2022-05-23 12:38:38 -0500

    feat: outputs when placing ships

--- lib/player_placement.rb
+++ lib/player_placement.rb
@@ -10,7 +10,32 @@ attr_reader :board, :ships_to_be_placed
   end
 
   def opening_prompt
-    return "You now need to lay out your #{@ships_to_be_placed.count} ships."
+    return "You now need to lay out your #{@ships_to_be_placed.length} ships.\n The #{@ships_to_be_placed[0].name} is #{@ships_to_be_placed[0].length} units long and the #{@ships_to_be_placed[1].name} is #{@ships_to_be_placed[1].length} units long."
+  end
+
+  def display_board(render_status = false)
+    @board.render
+  end
+
+  def ship_instructions
+    "Enter the squares for the #{@ships_to_be_placed[0].name} (#{@ships_to_be_placed[0].length} spaces):"
+  end
+
+  def user_input_array(input)
+    input.split(" ")
+  end
+
+  def check_user_input(ship, input_array)
+    check_input = user_input_array(input_array)
+    if @board.valid_placement?(ship, check_input)
+      @board.place(ship, check_input)
+      @ships_to_be_placed.shift
+    else
+      "Those are invalid coordinates. Please try again:"
+    end
+  end
+  def place_ship
+    @board.place(ship, input_array)
   end
 
 end
